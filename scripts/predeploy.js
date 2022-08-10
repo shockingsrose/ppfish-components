e1147a7cfe6021a73195f284d9c6941ee785386d
storm <847073376@qq.com>
2022-08-10 19:58:56 +0800

    feat: 接入workflow release-publish

---
+++ scripts/predeploy.js
@@ -0,0 +1,36 @@
+const { promises: fs } = require('fs');
+const path = require('path');
+const pkg = require('../package.json');
+const core = require('@actions/core')
+
+const changeLogPath = path.join(__dirname, '../CHANGELOG.md');
+
+const run = async () => {
+  const changeLogData = await fs.readFile(changeLogPath, { encoding: 'utf-8' })
+  const version = pkg.version;
+
+  /**获取最新版本changeLog */
+  const [, changeLog] = /(###\s+[\w.]+\n[\s\S]+?)###/.exec(changeLogData);
+
+  const outputs = { version, changeLog };
+
+  const isPrerelease = /[\d.]+-[\w.]+/.test(version);
+  outputs.prerelease = isPrerelease;
+
+  const getPreid = () => {
+    return version.replace(/[\d.-]/g, '')
+  }
+
+  /** 通过version判断应该发布什么类型的npm包 */
+  const preid = getPreid();
+  outputs.pubScript = preid ? `pub:${preid}` : 'pub';
+  console.log(`pubScript: ${outputs.pubScript}`);
+
+
+  Object.entries(outputs).forEach(([key, value]) => {
+    core.setOutput(key, value);
+  })
+
+}
+
+run();
