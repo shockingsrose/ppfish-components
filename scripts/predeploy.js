4659b10c75ae266e55e501cdb22e594df5c75905
storm <847073376@qq.com>
2022-08-12 19:59:49 +0800

    build: 增加shouldRelease，判断是否需要release

--- scripts/predeploy.js
+++ scripts/predeploy.js
@@ -1,12 +1,26 @@
 const { promises: fs } = require('fs');
 const path = require('path');
 const pkg = require('../package.json');
-const core = require('@actions/core')
+const core = require('@actions/core');
+const { promisify } = require('util')
+const { exec } = require('child_process');
+const execAsync = promisify(exec);
 
 // 判断是否要发布github page
 const { DEPLOY_GITHUB_PAGES } = process.env;
 
 const changeLogPath = path.join(__dirname, '../CHANGELOG.md');
+
+/**
+ * 检查当前版本是否需要发布
+ * @param {string} version 当前package.json的version
+ * @returns {boolean} true: 表示需要发布
+ */
+const checkVersionShouldRelease = async (version) => {
+  const { stdout } = await execAsync('git tag');
+
+  return !stdout.includes(version)
+}
 
 const run = async () => {
   const changeLogData = await fs.readFile(changeLogPath, { encoding: 'utf-8' })
@@ -20,15 +34,12 @@ const run = async () => {
   const isPrerelease = /[\d.]+-[\w.]+/.test(version);
   outputs.prerelease = isPrerelease;
 
-  const getPreid = () => {
-    return version.replace(/[\d.-]/g, '')
-  }
 
   /** 通过version判断应该发布什么类型的npm包 */
-  const preid = getPreid();
+  const preid = version.replace(/[\d.-]/g, '');
   outputs.pubScript = preid ? `pub:${preid}` : 'pub';
-  console.log(`pubScript: ${outputs.pubScript}`);
 
+  outputs.shouldRelease = await checkVersionShouldRelease(version);
 
   Object.entries(outputs).forEach(([key, value]) => {
     core.setOutput(key, value);
